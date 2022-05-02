f729e30b7e4d6b0c67e02c6920057c1b116c557b
Michal Lauer <michal.lauer.25@gmail.com>
2022-05-02 12:05:13 +0200

    Final citations

---
+++ run.R
@@ -0,0 +1,19 @@
+# Nahrání balíčků
+renv::activate("analyza/")
+renv::restore(prompt = F)
+rstudioapi::restartSession()
+renv::activate("analyza/")
+
+# Nastavení working directory pro render
+wd <- setwd(rstudioapi::getActiveDocumentContext()$path |> dirname())
+setwd(wd)
+
+# Kontrola dat
+if (!dir.exists("analyza/data")) {
+  stop(sprintf("Prve je nutné stáhnout datový soubor z %s do složky analyza/data",
+               "https://www.kaggle.com/datasets/mateusdmachado/csgo-professional-matches"))
+}
+
+# Renderování pomocných souborů
+rmarkdown::render("analyza/grafy.Rmd", quiet = T)
+rmarkdown::render("analyza/analyza.Rmd", quiet = T)
