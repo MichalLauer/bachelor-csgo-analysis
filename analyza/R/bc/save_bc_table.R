ff524a15987d5c46f9fe95d5516d2fcb50683d07
Michal <michal.lauer.25@gmail.com>
2022-05-08 15:44:24 +0200

    Finální verze

--- analyza/R/bc/save_bc_table.R
+++ analyza/R/bc/save_bc_table.R
@@ -6,45 +6,46 @@
 #' @param .lab Label
 #' @param .scale Poměr tabulky
 save_bc_table <- function(.tibble, .file, .cap, .lab,
-                          .scale = 1) {
-  
+                          .scale = 1, .rot = F) {
+
   # Pokud není zadaný label, použije se jako label název souboru
   # bez přípony
   if (is.na(.lab)) {
     .lab <- sprintf("tab:%s", str_extract(.file, "^.*(?=\\.tex)"))
   }
-  
+
   # Protože xtable neumí vypsat datum, převede se na text
   if ("date" %in% colnames(.tibble)) {
-    .tibble <- .tibble |> 
+    .tibble <- .tibble |>
       mutate(
         across(
           .cols = "date",
           .fns = as.character))
-    
+
   }
-  
+
   # Vytvoření pojmenované tabulky
   xtable <- xtable(
     .tibble,
     caption = .cap,
     label = .lab,
-    digits = 4 
+    digits = 4
   )
-  
+
   # V xtable existuje bug, kde při globálním nastavení decimálního
   # oddělovače na ',' se vypíše .scale s čárkou a LaTeX hází error
   options(OutDec = ".")
-  
+
   # Vytvoření cesty pro soubor
   .file = paste(TABLES_DIR, .file, sep = "/")
-  
+
   # Uložení tabulky do souboru
   print(xtable,
         file = .file,
-        scalebox = .scale
+        scalebox = .scale,
+        rotate.colnames = .rot
   )
-  
+
   # Znovunastavení decimálního oddělovače na ','
   options(OutDec = ",")
 }
